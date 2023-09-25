79b432209a38182603fd6e8175e4fb34672d9e88
Pedanticc <a.m.sakhravi@gmail.com>
2023-09-25 14:02:47 +0330

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="border-1 border-yellow border-solid">
+      <p className="text-white flex justify-center items-center">TODO LIST</p>
+    </div>
+  );
+}
+
+export default App;
