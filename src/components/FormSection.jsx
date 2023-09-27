8cf4ca093225d0831440107b517668f92c47f473
Pedanticc <a.m.sakhravi@gmail.com>
2023-09-27 21:40:22 +0330

    Tasks section wireframe - Changed background img - Made with spite - Task discription & title headers

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -2,26 +2,51 @@ export const FormSection = () => {
   return (
     <div className="h-100% min-w-335px max-w-335px flex flex-col border-r-1 border-r-solid border-r-#c7c7c711 ">
       <p className="text-#d2d0dd font-RebondBold text-25px">To Do Hive</p>
-
-      <form action="" className="mt-50px w-100%">
-        <input type="text" className="text-box mb-30px h-40px" />
-        <input type="text" className="text-box h-200px" />
+      <p className="text-#9b96b0 font-RebondThin text-18px mt-5px">
+        An unnecessarily beautified To Do List
+      </p>
+      <form action="" className="mt-40px w-100%">
+        <p className="text-#9b96b0 font-RebondRegular p-l-10px mb-10px text-14px">
+          Task Title:
+        </p>
+        <input
+          type="text"
+          className="text-box mb-20px h-40px text-#9b96b0 p-10px font-RebondThin rounded-30px"
+        />
+        <p className="text-#9b96b0 font-RebondRegular p-l-10px mb-10px text-14px">
+          Task Discription:
+        </p>
+        <input
+          type="text"
+          className="text-box h-200px text-#9b96b0 p-10px font-RebondThin rounded-30px"
+        />
       </form>
       <ul className="w-90% mt-20px h-fit flex justify-center items-center gap-x-1 gap-y-1.5 flex-wrap">
-        <li className="btn-blur w-100px h-30px font-InterThin flex justify-center items-center rounded-10px  text-#9b96b0 bg-emerald-900/20 backdrop-blur-sm cursor-pointer">
+        <li className="btn-blur w-100px h-30px font-InterThin flex justify-center items-center rounded-10px  text-#9b96b0  backdrop-blur-sm cursor-pointer create-btn">
           Important
         </li>
-        <li className="btn-blur w-80px h-30px font-InterThin flex justify-center items-center rounded-10px text-#9b96b0 bg-emerald-900/20 backdrop-blur-sm cursor-pointer">
+        <li className="btn-blur w-80px h-30px font-InterThin flex justify-center items-center rounded-10px text-#9b96b0 backdrop-blur-sm cursor-pointer create-btn">
           Hard
         </li>
-        <li className="btn-blur w-80px h-30px font-InterThin flex justify-center items-center rounded-10px text-#9b96b0 bg-emerald-900/20 backdrop-blur-sm cursor-pointer">
+        <li className="btn-blur w-80px h-30px font-InterThin flex justify-center items-center rounded-10px text-#9b96b0 backdrop-blur-sm cursor-pointer create-btn">
           Regular
         </li>
       </ul>
       <div className="w-100% flex justify-center p-r-35px mt-35px">
-        <div className="w-100px h-50px color-#7060c2 create-btn font-RebondRegular flex justify-center items-center rounded-40px">
+        <div className="w-200px h-50px color-#7060c2 create-btn font-RebondRegular flex justify-center items-center rounded-40px">
           Create
         </div>
+      </div>
+      <div className="w-100% flex justify-center p-r-35px mt-30px">
+        <p className="text-#9b96b0 font-RebondThin flex justify-center text-13px">
+          Made with Spite by{" "}
+          <a
+            className="color-#7060c2 p-l-1 neon underline"
+            href="https://github.com/Pedanticc"
+          >
+            Pedantic
+          </a>
+        </p>
       </div>
     </div>
   );
--- src/components/TasksSection.jsx
+++ src/components/TasksSection.jsx
@@ -1,3 +1,15 @@
 export const TasksSection = () => {
-  return <div className="w-100%"></div>;
+  return (
+    <div className="w-100% p-l-20px">
+      <p className="color-#d2d0dd font-RebondBold text-25px border-b-1 border-b-solid border-b-#c7c7c711 p-b-10px">
+        List
+      </p>
+      <ul className="h-93% flex flex-col p-10px gap-10px list-none	">
+        <li className="h-80px min-h-80px create-btn rounded-10px"></li>
+        <li className="h-80px min-h-80px create-btn rounded-10px"></li>
+        <li className="h-80px min-h-80px create-btn rounded-10px"></li>
+        <li className="h-80px min-h-80px create-btn rounded-10px"></li>
+      </ul>
+    </div>
+  );
 };
