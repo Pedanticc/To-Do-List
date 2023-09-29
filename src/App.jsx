ff9ca0f186e9bf91bd9bf671ecba1b45033c97c1
Pedanticc <a.m.sakhravi@gmail.com>
2023-09-29 12:29:21 +0330

    Added a border to the task container - Width of the main container slightly increased - Hover effect - X overflow hidden

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { TasksSection } from "./components/TasksSection";
 
 function App() {
   return (
-    <div className="w-880px min-w-880px h-650px rounded-8px border-1 border-solid border-#ffffff1a p-35px flex drop-shadows frosted-container">
+    <div className="w-950px min-w-880px h-650px rounded-8px border-1 border-solid border-#ffffff1a p-35px flex drop-shadows frosted-container">
       <FormSection></FormSection>
       <TasksSection></TasksSection>
     </div>
